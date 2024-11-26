ae1444044a232516a6e3b118e559f2e236deeff4
sharmila657 <sharmilajirel8@gmail.com>
2024-11-26 15:36:41 +0545

    loops practise

--- loops/for.js
+++ loops/for.js
@@ -1,4 +1,9 @@
-debugger
-for(let i = 0; i<=5; i++){
-    console.log("hello")
+// debugger
+// for(let i = 0; i<=5; i++){
+//     console.log("hello nepal")
+// }
+
+let fruits = ["apple","banana","mango","cherry"];
+for(let i=0; i<fruits.length; i++){
+    console.log("i love " + fruits)
 }
