5bfc0581284f83236c0e90adbdb644bc8c52f9fd
sharmila657 <sharmilajirel8@gmail.com>
2024-11-27 21:42:16 +0545

    iteration -- doesnt work

--- loops/for.js
+++ loops/for.js
@@ -3,7 +3,12 @@
 //     console.log("hello nepal")
 // }
 
-let fruits = ["apple","banana","mango","cherry"];
-for(let i=0; i<fruits.length; i++){
-    console.log("i eat" + " " + fruits[i] +" everyday.")
+// let fruits = ["apple","banana","mango","cherry"];
+// for(let i=0; i<fruits.length; i++){
+//     console.log("i eat" + " " + fruits[i] +" everyday.")
+// }
+
+let names = ["Sharmi","Bindu"];
+for(let i=0; i<names.length; i--){
+    console.log(names[i])
 }
