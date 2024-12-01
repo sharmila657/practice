d7e69c998cd1b1bff0584b6db207d342f4351212
sharmila657 <sharmilajirel8@gmail.com>
2024-12-01 21:22:10 +0545

    see dfference

--- array-1/practse.js
+++ array-1/practse.js
@@ -1,4 +1,3 @@
 let arrary1 = ["dog","cat","parrot"];
-let array2 = arrary1.push("horse") //adds horse at the end of array1
-console.log(array2) //gves length of array1 not the whole array
+arrary1.push("horse") //adds horse at the end of array1
 console.log(arrary1) // gves new updated array
