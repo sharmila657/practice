b46bed34ffa293d7c77b88cf2c35960efba3dd67
sharmila657 <sharmilajirel8@gmail.com>
2024-12-03 21:48:23 +0545

    looping in array

--- array-1/practse.js
+++ array-1/practse.js
@@ -1,4 +1,9 @@
 let arrary1 = ["dog","cat","parrot"];
-let array2 = arrary1.push("horse") //adds horse at the end of array1
-console.log(array2) //gves length of array1 not the whole array
-console.log(arrary1) // gves new updated array
+// let array2 = arrary1.push("horse") //adds horse at the end of array1
+// console.log(array2) //gves length of array1 not the whole array
+// console.log(arrary1) // gves new updated array
+
+
+for(let i=0; i< arrary1.length; i++){
+    console.log(arrary1[i])
+}
