0c519385691b5bd2ebe205f4769f4d62fb40b85a
sharmila657 <sharmilajirel8@gmail.com>
2024-12-07 20:46:50 +0545

    array practce

--- array-1/practse.js
+++ array-1/practse.js
@@ -3,7 +3,11 @@ let arrary1 = ["dog","cat","parrot"];
 // console.log(array2) //gves length of array1 not the whole array
 // console.log(arrary1) // gves new updated array
 
+console.log(arrary1[0])
+
 
 for(let i=0; i< arrary1.length; i++){
     console.log(arrary1[i])
 }
+arrary1.push("elephant")
+console.log(arrary1)
